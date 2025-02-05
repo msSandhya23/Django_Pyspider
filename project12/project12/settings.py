df1ad5c140646b49e92aea0020ed5e14274a2f99
msSandhya23 <sandhyabehera33090@gmail.com>
2025-02-05 10:49:47 +0530

    use Dynamic way

--- project12/project12/settings.py
+++ project12/project12/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = 'C:\\Users\\sandh\\OneDrive\\Desktop\\django2\\project12\\templates'
+#TEMPLATES_DIR = 'C:\\Users\\sandh\\OneDrive\\Desktop\\django2\\project12\\templates'
 
-
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
