c9411045afbec56c77fd16f72810a6e3bb0a2381
msSandhya23 <sandhyabehera33090@gmail.com>
2025-02-14 11:46:17 +0530

    create url navigation(using generic url navigation)

---
+++ project7/app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Sandhya(request):
+    return render(request,'sandhya.html')
+def Soumya(request):
+    return render(request,'soumya.html')
