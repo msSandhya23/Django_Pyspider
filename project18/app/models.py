a8b709b0cc430b840f18e6ff4929c8c283f8c0ec
msSandhya23 <sandhyabehera33090@gmail.com>
2025-03-05 11:49:43 +0530

    add email field to Webpage model and implement __str__ methods for models

--- project18/app/models.py
+++ project18/app/models.py
@@ -4,13 +4,23 @@ from django.db import models
 class Topic(models.Model):
     topic_name = models.CharField(max_length=264, unique=True)
     
+    def __str__(self):
+        return self.topic_name
+    
 class Webpage(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField(max_length=264, unique=True)
     url = models.URLField()
+    email = models.EmailField(max_length=264,default='hai@gmail.com')
+    
+    def __str__(self):
+        return self.name
     
 
 class AccessRecord(models.Model):
     name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
     date = models.DateField()
     author = models.CharField(max_length=264)
+    
+    def __str__(self):
+        return self.author
