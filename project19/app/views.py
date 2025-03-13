6f6a19344dfc36f9b35e4e243ee677bc583a0903
msSandhya23 <sandhyabehera33090@gmail.com>
2025-03-13 12:05:26 +0530

    add insert_emp view for employee creation with input validation

--- project19/app/views.py
+++ project19/app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from app.models import *
+from django.http import HttpResponse
 # Create your views here.
+def insert_emp(request):
+    eno = int(input('Enter employee number: '))
+    ename = input('Enter employee name: ')
+    job = input('Enter employee job: ')
+    sal = float(input('Enter employee salary: '))
+    comm = input('Enter employee commission: ')
+    if comm:
+        comm = float(comm)
+    else:
+        comm = None
+    hiredate = input('Enter employee hiredate: ')
+    deptno = int(input('Enter employee department number: '))
+    DO = Dept.objects.get(deptno=deptno)
+    mgr = input('Enter employee manager NO: ')
+    if mgr:
+        MO = Emp.objects.get(empno = int(mgr))
+    else:
+        MO = None
+    TEO = Emp.objects.get_or_create(empno = eno, ename = ename, job = job, mgr = MO, hiredate = hiredate, sal = sal, comm = comm, deptno = DO)
+    if TEO[1]:
+        return HttpResponse('Employee added successfully')
+    else:
+        return HttpResponse('Employee already exists')
+    
+    
+    
--- project19/project19/urls.py
+++ project19/project19/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("insert_emp/",insert_emp,name = "insert_emp"),
 ]
