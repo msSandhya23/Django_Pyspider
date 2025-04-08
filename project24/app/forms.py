e50937e64add27f84feaacbcef6c7ff108d9f75f
msSandhya23 <sandhyabehera33090@gmail.com>
2025-04-08 12:01:11 +0530

    update StudentForm to include Address field with textarea widget

--- project24/app/forms.py
+++ project24/app/forms.py
@@ -2,6 +2,6 @@ from django import forms
 class StudentForm(forms.Form):
     StName = forms.CharField()
     StAge = forms.IntegerField()
-    #Address = forms.CharField(widget=forms.Textarea)
+    Address = forms.CharField(widget=forms.Textarea())
     Password = forms.CharField(widget=forms.PasswordInput)
     
